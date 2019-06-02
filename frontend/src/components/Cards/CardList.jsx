9246fa2e2e67410592141ed59c0655f96b4576df
Anas Laaroussi Alami <anaslaaroussialami@gmail.com>
2019-06-02 10:41:55 +0000

    init front end project and add Header

---
+++ frontend/src/components/Cards/CardList.jsx
@@ -0,0 +1,7 @@
+// export const CardList = ({users}) => {
+
+//     users.map((user)=> {
+
+//     })
+
+// }
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const styles = {
+  header: {
+    height: 70,
+    textAlign: "center",
+    lineHeight: "70px",
+    backgroundColor: "#4B4242",
+    color: "#fff"
+  }
+};
+
+export const Header = () => {
+  return <header style={styles.header}>Home</header>;
+};
